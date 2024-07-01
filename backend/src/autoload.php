7a128cc557ad3be5b91b4ae944bd48b22d725438
markoblagdan <mblagdan@gmail.com>
2024-07-01 12:15:24 +0200

    debug autoloader...

--- backend/src/autoload.php
+++ backend/src/autoload.php
@@ -3,5 +3,14 @@
 
 set_include_path(__ROOT__."/src/");
 
+function my_autoload($class){
+    $file = __ROOT__.'/src/' . $class . '.php';
+    print("Looking for file: ".$file."\n");
+    if(file_exists($file)) {
+       print("Found file: ".$file."\n");
+       include $file;
+    }
+}
+
 spl_autoload_extensions('.php');
-spl_autoload_register();
+spl_autoload_register("my_autoload");
